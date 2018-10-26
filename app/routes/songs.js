71c017d046a28ea27264413b7e9203cdf160b1e6
Alexander Rau <alexanderrau86@yahoo.com>
2018-10-26 10:01:54 -0600

    Ad songs route and clean up application

--- app/routes/songs.js
+++ app/routes/songs.js
@@ -1,4 +1,32 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
+
+let Song = EmberObject.extend({
+  title: '',
+  band: '',
+  rating: 0
+});
 
 export default Route.extend({
+  model() {
+    let bornToBeHated = Song.create({
+      title: 'Born To Be Hated',
+      band: 'Hocico',
+      rating: 4
+    });
+
+    let strapMeDown = Song.create({
+      title: 'Strap Me Down',
+      band: 'Leæther Strip',
+      rating: 4
+    });
+
+    let megalomaniac = Song.create({
+      title: 'Megalomaniac',
+      band: 'KMFDM',
+      rating: 5
+    });
+
+    return [bornToBeHated, strapMeDown, megalomaniac];
+  }
 });
